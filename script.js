3f104ab68fb9b94e07bc113955678c67c6df092b
Tan Thinh <thinh522004@gmail.com>
2024-11-15 19:36:42 +0700

    Xóa file ten_file

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const logoImage = document.getElementById("img-logo");
-
-  logoImage.addEventListener("dragstart", function (event) {
-    event.preventDefault();
-  });
-});
